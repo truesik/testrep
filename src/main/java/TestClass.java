8d906c82fec9bacc72ed1c30125f91c0d60745c1
truesik <truesik@gmail.com>
2016-06-07 22:10:23 +0300

    commit

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,11 +1,17 @@
 public class TestClass {
     private String name;
+    private int days;
 
-    public TestClass(String name) {
+    public TestClass(String name, int days) {
         this.name = name;
+        this.days = days;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getDays() {
+        return days;
     }
 }
